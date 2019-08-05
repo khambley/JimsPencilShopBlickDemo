fddf797cb120915f7f25db02bb7511bd63f103dc
khambley <superdesigngirl@mac.com>
2019-08-04 22:11:25 -0500

    Added Product Controller class with backend logic and business rules

---
+++ JimsPencilShopBlickDemo/Controllers/ProductController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using JimsPencilShopBlickDemo.Models;
+
+namespace JimsPencilShopBlickDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        ProductDataAccessLayer objProduct = new ProductDataAccessLayer();
+
+        [HttpGet]
+        [Route("api/Product/Index")]
+        public IEnumerable<Product> Index()
+        {
+            return objProduct.GetAllProducts();
+        }
+
+        [HttpPost]
+        [Route("api/Product/Create")]
+        public int Create(Product product)
+        {
+            return objProduct.CreateProduct(product);
+        }
+
+        [HttpGet]
+        [Route("api/Product/Details/{id}")]
+        public Product Details(int id)
+        {
+            return objProduct.GetProductDetails(id);
+        }
+
+        [HttpPut]
+        [Route("api/Product/Edit")]
+        public int Edit(Product product)
+        {
+            return objProduct.UpdateProduct(product);
+        }
+
+        [HttpDelete]
+        [Route("api/Product/Delete/{id}")]
+        public int Delete (int id)
+        {
+            return objProduct.DeleteProduct(id);
+        }
+
+        [HttpGet]
+        [Route("api/Product/GetBuyerList")]
+        public IEnumerable<Buyer> Details()
+        {
+            return objProduct.GetBuyers();
+        }
+    }
+}
